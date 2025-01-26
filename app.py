565c8c96ebfcddfebc3d15dc8b87b15ba462bdc7
lichyyyyy <thelich00509@gmail.com>
2025-01-26 20:02:48 +0100

    will you look at that :) i added a flag hmmm wait... i gotta fix something
    it looks like i made a mistake but no worries it's all fixed ;)

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template_string("<helllllo and welcome to my webserver!>")
+    try:
+        with open('./flag.txt', 'r') as file:
+            content = file.read()
+    except FileNotFoundError:
+        content = "File not found."
+    return render_template('index.html', content=content)
 
 if __name__ == '__main__':
     app.run(debug=True)
